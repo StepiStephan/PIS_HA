be7876bb2f7489c23649ecf9831c9ec52a1c65be
Administrator <stonedlover@darkdestinylp.de>
2016-12-08 12:17:06 +0100

    Init Project

---
+++ src/pis/hu2/client/LaunchClient.java
@@ -0,0 +1,7 @@
+package pis.hu2.client;
+
+/**
+ * Created by Dijivu on 08.12.2016.
+ */
+public class LaunchClient {
+}
---
+++ src/pis/hu2/server/LaunchServer.java
@@ -0,0 +1,7 @@
+package pis.hu2.server;
+
+/**
+ * Created by Dijivu on 08.12.2016.
+ */
+public class LaunchServer {
+}
